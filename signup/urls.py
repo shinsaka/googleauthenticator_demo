7795ab5cd48eb2c1b2a110c69e9e86530aa912c8
shinsaka <shinx1265@gmail.com>
2016-10-01 15:27:49 +0900

    remove no need files

--- signup/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-urlpatterns = [
-    url(r'^$', views.signup, name='signup-index'),
-    url(r'^confirm$', views.confirm_token, name='confirm_token'),
-    url(r'^success$', views.success, name='signup_success'),
-]
